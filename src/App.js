67628ecbafba65c9594299a09112455033d6706f
Ba Quan Win10 <quannar178@gmail.com>
2021-08-15 22:45:26 +0700

    Use hook & setting eslint

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
-
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt me!"),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bizd",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Sudo",
-//       animal: "Dog",
-//       breed: "Wheaten Terrier",
-//     }),
-//   ]);
-// };
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bizd" breed="Cockatiel" />
-      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+      <SearchParams />
     </div>
   );
 };
