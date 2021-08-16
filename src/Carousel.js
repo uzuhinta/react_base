cf6bf9c2602db7d8f74dc20f24d85cf8522cf964
Ba Quan Win10 <quannar178@gmail.com>
2021-08-16 10:40:08 +0700

    Interactive class component

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,9 +2,17 @@ import { Component } from "react";
 
 class Carousel extends Component {
   state = { active: 0 };
+
   static defaultProps = {
     images: ["http://pets-v2.dev-apis.com/pets/none.jpg"],
   };
+
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -13,9 +21,12 @@ class Carousel extends Component {
         <img src={images[active]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
               key={photo}
               src={photo}
+              data-index={index}
+              onClick={this.handleIndexClick}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />
