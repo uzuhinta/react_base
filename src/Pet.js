2e393cf6537c1e7aa9577a3cdc1f4c9fa544e17f
Ba Quan Win10 <quannar178@gmail.com>
2021-08-16 10:31:47 +0700

    Manage state in a class component

--- src/Pet.js
+++ src/Pet.js
@@ -7,7 +7,9 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
   }
   return (
     <Link to={`/details/${id}`} className="pet">
-      <div className="image-container">{/* <img src={hero} /> */}</div>
+      <div className="image-container">
+        <img src={hero} alt={animal} />
+      </div>
       <div className="info">
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
