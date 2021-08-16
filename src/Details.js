eca61a83fcf7ade0c6a55a1deeb0dd4de6ea55f9
Ba Quan Win10 <quannar178@gmail.com>
2021-08-16 10:20:35 +0700

    Setup babel & eslint for class properties

--- src/Details.js
+++ src/Details.js
@@ -2,11 +2,8 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 class Details extends Component {
-  constructor() {
-    super();
+  state = { loading: true };
 
-    this.state = { loading: true };
-  }
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
