a880b346a5afd84724ccc0e46bf5ebb3a93cadb4
Ba Quan Win10 <quannar178@gmail.com>
2021-08-16 09:43:06 +0700

    Add react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import ReactDOM from "react-dom";
 import { StrictMode } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt me!</h1>
-      <SearchParams />
+      <Router>
+        <Link to="/">
+          <h1>Adopt me!</h1>
+        </Link>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
--- src/Pet.js
+++ src/Pet.js
@@ -1,19 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Pet = ({ name, animal, breed, images, location, id }) => {
   let hero = "http://pets-v2.dev-apis.com/pets/none.jpg";
   if (images.length) {
     hero = images[0];
   }
   return (
-    <a href={`/details/${id}`} className="pet">
-      <div className="image-container">
-        {/* <img src={hero} /> */}
-
-      </div>
+    <Link to={`/details/${id}`} className="pet">
+      <div className="image-container">{/* <img src={hero} /> */}</div>
       <div className="info">
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 };
 export default Pet;
