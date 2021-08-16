b3ac2e1859a335d34a216927fa49c67b668f50b8
Ba Quan Win10 <quannar178@gmail.com>
2021-08-16 13:40:17 +0700

    Context consumer

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoudary";
+import ThemeContext from "./ThemeContext";
 class Details extends Component {
   state = { loading: true };
 
@@ -22,8 +23,6 @@ class Details extends Component {
     );
   }
 
-  
-
   render() {
     if (this.state.loading) {
       return <h2>Loading</h2>;
@@ -36,7 +35,10 @@ class Details extends Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => <button style={{backgroundColor: theme}}>Adopt {name}</button>}
+            {/* {(theme) => <button style={{backgroundColor: theme[0]}}>Adopt {name}</button>} */}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
