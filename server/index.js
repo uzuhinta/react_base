af256455bf5b2da2732c3818120f26345f711d21
Ba Quan Win10 <quannar178@gmail.com>
2021-08-17 11:08:22 +0700

    streaming markup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { renderToString } from "react-dom/server";
+import { renderToNodeStream } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
 import fs from "fs";
 import App from "../src/App";
@@ -14,6 +14,7 @@ const app = express();
 
 app.use("/dist", express.static("dist"));
 app.use((req, res) => {
+  res.write(parts[0]);
   const staticContext = {};
   console.log("@@@");
   const reactMarkup = (
@@ -22,9 +23,14 @@ app.use((req, res) => {
     </StaticRouter>
   );
 
-  res.status(staticContext.statusCode || 200);
-  res.send(`${parts[0]}${renderToString(reactMarkup)}${parts[1]}`);
-  res.end();
+  const stream = renderToNodeStream(reactMarkup);
+  stream.pipe(res, { end: false });
+
+  stream.on("end", () => {
+    res.status(staticContext || 200);
+    res.write(parts[1]);
+    res.end();
+  });
 });
 
 console.log(`listening on http://localhost:${PORT}`);
